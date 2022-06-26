7b9e17eed9658ff8346eb64112595c747e80b847
kvgd28 <kvgd28@gmail.com>
2022-06-26 14:12:54 +0530

    Basic implementation of slot picker

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import SelectSlot from "./SelectSlot";
 
 function About() {
+  const address = {
+    name: "gangadhar",
+    primaryAddressLine: "vinayaka layout",
+    secondaryAddressLine: "Kamakshipalya",
+    landmark: "traffic police station",
+    city: "bangalore",
+    state: "karnataka",
+    pincode: 560079
+  };
   return (
     <>
-      <h1>About</h1>
+      <SelectSlot mobileNumber="8639955673" address={JSON.stringify(address)} />
     </>
   );
 }
--- src/SelectSlot.jsx
+++ src/SelectSlot.jsx
@@ -1,14 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import SelectAddress from "./SelectAddress";
+import DatePicker from "react-date-picker";
+import SlotPicker from "slotpicker";
 
 function SelectSlot(props) {
+  const [date, setDate] = useState();
+  const [minDate, setMinDate] = useState(new Date());
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() + 3);
+  const [maxDate, setMaxDate] = useState(currentDate);
+  const [startTime, setStartTime] = useState();
+  const [isTimeSelected, setIsTimeSelected] = useState(false);
+
+  function handleTimeSelect(fromTime) {
+    setStartTime(fromTime);
+    setIsTimeSelected(true);
+  }
+
   return (
     <>
       <p>
         Select Slot here for {props.mobileNumber} and {props.address}
       </p>
       <br />
+      <DatePicker
+        onChange={setDate}
+        value={date}
+        minDate={minDate}
+        maxDate={maxDate}
+        monthPlaceholder="mm"
+        dayPlaceholder="dd"
+        yearPlaceholder="yyyy"
+        format="y-MM-dd"
+      />
+      <br />
+      {date && (
+        <SlotPicker
+          // Required, interval between two slots in minutes, 30 = 30 min
+          interval={60}
+          // Required, when user selects a time slot, you will get the 'from' selected value
+          onSelectTime={(from) => handleTimeSelect(from)}
+          // Optional, array of unavailable time slots
+          unAvailableSlots={["10:00", "15:30"]}
+          // Optional, 8AM the start of the slots
+          from={"10:00"}
+          // Optional, 09:00PM the end of the slots
+          to={"20:00"}
+          // Optional, 01:00 PM, will be selected by default
+          //defaultSelectedTime={"13:00"}
+          // Optional, selected slot color
+          selectedSlotColor="#F09999"
+          // Optional, language of the displayed text, default is english (en)
+          lang="en"
+        />
+      )}
+      {isTimeSelected && <div>{startTime.$H}</div>}
       <NavLink to="/selectAddress" className="btn-get-started">
         Back
       </NavLink>
