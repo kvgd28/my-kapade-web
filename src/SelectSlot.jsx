fe97b9e7daf8cadb50c363b0662adb7f4b0a9258
kvgd28 <kvgd28@gmail.com>
2022-06-27 23:13:09 +0530

    Fetch slots from backend

--- src/SelectSlot.jsx
+++ src/SelectSlot.jsx
@@ -25,20 +25,22 @@ function SelectSlot(props) {
   // this useEffect will run once
   // similar to componentDidMount()
   useEffect(() => {
+    var epochStart = minDate.getTime(); // start of the day
+    var epochEnd = maxDate.getTime() + 86400000; // end of the day
     fetch(
-      `https://booking-service-kdewilj24a-uc.a.run.app/fetchfreeslots?latitude=0.0&longitude=0.0&durationInMinutes=${slotDurationInMinutes}&items[0].displayName=ALL`
+      `https://booking-service-kdewilj24a-uc.a.run.app/fetchinvalidslots?slotDurationInMinutes=60&startTime=${epochStart}&endTime=${epochEnd}`
     )
       .then((res) => res.json())
       .then(
         (slots) => {
-          setAddresses(result);
+          setInvalidSlots(slots);
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {}
       );
-  }, []);
+  }, [minDate, maxDate]);
 
   return (
     <>
@@ -57,6 +59,7 @@ function SelectSlot(props) {
         format="y-MM-dd"
       />
       <br />
+      {date && JSON.stringify(invalidSlots)}
       {date && (
         <SlotPicker
           // Required, interval between two slots in minutes, 30 = 30 min
@@ -64,7 +67,23 @@ function SelectSlot(props) {
           // Required, when user selects a time slot, you will get the 'from' selected value
           onSelectTime={(from) => handleTimeSelect(from)}
           // Optional, array of unavailable time slots
-          unAvailableSlots={["10:00", "15:30"]}
+          unAvailableSlots={invalidSlots
+            .filter(
+              (slot) =>
+                new Date(slot.startTimeInEpoch).getDate() === date.getDate()
+            )
+            .map(
+              (slot) =>
+                "" +
+                new Date(slot.startTimeInEpoch).getHours() +
+                ":" +
+                new Date(slot.startTimeInEpoch)
+                  .getMinutes()
+                  .toLocaleString("en-US", {
+                    minimumIntegerDigits: 2,
+                    useGrouping: false
+                  })
+            )}
           // Optional, 8AM the start of the slots
           from={"10:00"}
           // Optional, 09:00PM the end of the slots
